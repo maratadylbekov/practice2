8dfd52501b10259d2d2d4d48bceda5680f4b1393
Marat Adylbekov <maratadylbekov@gmail.com>
2024-03-30 14:20:38 +0600

    first test commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const category = 'toys';
+
+console.log(`http://someurl.com/${category}/5`);
+
+const user = 'Denzel';
+
+alert(`Hello, ${user}!`);
+
+// console.log('Hello, World!');
+
+// let arr = ['orange.png', 'wine.jpg', 'apple.bmp', 'cat', [], {}];
+// console.log(arr[4]);
+
+// const obj = {
+//     name: 'John',
+//     age: 23,
+//     isMarried: false
+// };
+// console.log(obj.age);
+
+// const res = confirm('Are you here?');
+// console.log(res);
+
+// const answer = prompt('Are you 18?', '18');
+// console.log(answer);
+
+// const answers = [];
+
+// answers[0] = prompt('What is your name?', 'John');
+// answers[1] = prompt('What is your surname?', 'Washington');
+// answers[2] = prompt('How old are you?', '23');
+
+// document.write(answers);
